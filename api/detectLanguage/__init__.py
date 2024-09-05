db49038609694c55a0a562a43d637c29e7095c6d
sithukaungset <sithukaungset27@gmail.com>
2024-09-05 14:12:49 +0900

    RAG hack agent

---
+++ api/detectLanguage/__init__.py
@@ -0,0 +1,44 @@
+import logging
+import requests
+import json
+import os
+
+import azure.functions as func
+
+endpoint = os.getenv("TEXT_ANALYTICS_ENDPOINT")
+subscription_key = os.getenv("TEXT_ANALYTICS_KEY")
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    apiUrl = f'{endpoint}/text/analytics/v3.2-preview.1/languages'
+    text = req.params.get('text')
+
+    requestBody = {
+        'documents': [
+        {
+            'id': '1',
+            'text': text
+        }
+        ]
+    }
+
+    requestOptions = {
+        'headers': {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key
+        },
+        'data': json.dumps(requestBody)
+    }
+
+    response = requests.post(apiUrl, **requestOptions)
+    data = response.json()
+    language_code = data['documents'][0]['detectedLanguage']['iso6391Name']
+
+    language_to_voice = {
+        "en": "en-US",
+        "ko": "ko-KR"
+}
+
+    if response.status_code == 200:
+        return func.HttpResponse(language_to_voice[language_code], status_code=200)
+    else:
+        return func.HttpResponse(response.status_code)
